acf7b601f8e2ab868a650c37ce9c61b29ed31c15
Ariel Barmat <abarmat@decentraland.org>
2018-07-25 18:28:09 -0300

    chore: add buidler config

--- buidler-config.js
+++ buidler-config.js
@@ -1,1 +1,22 @@
-module.exports = {};
+module.exports = {
+  networks: {
+    livenet: {
+      host: 'localhost',
+      port: 8545,
+      gas: 70000000,
+      network_id: '*' // Match any network id
+    },
+    development: {
+      host: 'localhost',
+      port: 8545,
+      gas: 100000000,
+      network_id: '*' // Match any network id
+    },
+    ropsten: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 3, // official id of the ropsten network
+      gas: 30000000
+    }
+  }
+}
