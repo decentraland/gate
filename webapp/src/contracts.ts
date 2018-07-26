257c6944dd777d72a0c950a99c624feb9c760280
Juan Cazala <juan@decentraland.org>
2018-07-26 19:01:26 -0300

    fix: added missing index

--- webapp/src/contracts.ts
+++ webapp/src/contracts.ts
@@ -1,24 +1,24 @@
-import { contracts /* Contract */ } from 'decentraland-eth'
+import { Contract, contracts } from 'decentraland-eth'
 import { env } from 'decentraland-commons'
-// import { fulfillContractMethods } from 'decentraland-eth/dist/contracts/verification'
-// const { abi } = require('./artifacts/truffle/DecentralandInvite.json')
+import { fulfillContractMethods } from 'decentraland-eth/dist/contracts/verification'
+const { abi } = require('./artifacts/truffle/DecentralandInvite.json')
 
 const manaToken = new contracts.MANAToken(
   env.get('REACT_APP_MANA_TOKEN_CONTRACT_ADDRESS')
 )
 
-class DecentralandInvite /* extends Contract */ {
-  // constructor(address: string) {
-  //   super(address, abi)
-  //   fulfillContractMethods(this, abi)
-  // }
+class DecentralandInvite extends Contract {
+  constructor(address: string) {
+    super(address, abi)
+    fulfillContractMethods(this, abi)
+  }
   getContractName() {
     return 'DecentralandInvite'
   }
 }
 
-const invite = new DecentralandInvite() as any
-/* env.get('REACT_APP_MANA_TOKEN_CONTRACT_ADDRESS'),
-  abi */
+const invite = new DecentralandInvite(
+  env.get('REACT_APP_MANA_TOKEN_CONTRACT_ADDRESS')
+) as any
 
 export { manaToken, invite }
