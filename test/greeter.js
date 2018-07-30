cc1769b69bd38ef02847c9f3fde673d744501524
Ariel Barmat <abarmat@decentraland.org>
2018-07-30 03:40:28 -0300

    feat: make contract pausable for transfers

--- test/greeter.js
+++ test/greeter.js
@@ -1,7 +1,71 @@
-const Invite = artifacts.require("DecentralandInvite");
+const BigNumber = web3.BigNumber
 
-describe("Invite contract", () => {
-  it("Should return the right greeting", async () => {
-    const invite = await Invite.new();
-  });
-});
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should()
+
+const EVMRevert = 'VM Exception while processing transaction: revert'
+
+const Invite = artifacts.require('DecentralandInvite')
+
+contract('Invite', function([_, owner, inviter, user, otherUser]) {
+  let invite
+  const gasParams = {
+    gas: 6e6,
+    gasPrice: 21e9
+  }
+
+  async function allowAndInvite(inviter) {
+    await invite.allow(inviter, 1, { from: owner })
+    await invite.invite(user, '', { from: inviter, ...gasParams })
+    return (await invite.totalSupply()) - 1
+  }
+
+  beforeEach(async function() {
+    // Create contract
+    invite = await Invite.new({ from: owner })
+  })
+
+  describe('Paused', function() {
+    it('Should throw if paused and transfer tried', async function() {
+      const tokenId = await allowAndInvite(inviter, user)
+
+      // Pause the contract
+      await invite.pause({ from: owner })
+
+      // transferFrom
+      await invite
+        .transferFrom(user, otherUser, tokenId, { from: user, ...gasParams })
+        .should.be.rejectedWith(EVMRevert)
+
+      // safeTransferFrom
+      await invite
+        .safeTransferFrom(user, otherUser, tokenId, {
+          from: user,
+          ...gasParams
+        })
+        .should.be.rejectedWith(EVMRevert)
+    })
+
+    it('Should transferFrom if unpaused and transfer tried', async function() {
+      const tokenId = await allowAndInvite(inviter, user)
+
+      // transferFrom
+      await invite.transferFrom(user, otherUser, tokenId, {
+        from: user,
+        ...gasParams
+      })
+      const tokenOwner1 = await invite.ownerOf(tokenId)
+      tokenOwner1.should.be.equal(otherUser)
+
+      // safeTransferFrom
+      await invite.safeTransferFrom(otherUser, user, tokenId, {
+        from: otherUser,
+        ...gasParams
+      })
+      const tokenOwner2 = await invite.ownerOf(tokenId)
+      tokenOwner2.should.be.equal(user)
+    })
+  })
+})
