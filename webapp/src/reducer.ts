5bfcbac285b19a45d2672927cb4e475ed24f21a6
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-08-01 11:29:50 +0200

    fix: enable local storage

--- webapp/src/reducer.ts
+++ webapp/src/reducer.ts
@@ -4,16 +4,21 @@ import { routerReducer as router } from 'react-router-redux'
 import { RootState } from 'types'
 import { walletReducer as wallet } from '@dapps/modules/wallet/reducer'
 import { translationReducer as translation } from '@dapps/modules/translation/reducer'
-import { storageReducer as storage } from '@dapps/modules/storage/reducer'
+import {
+  storageReducer as storage,
+  storageReducerWrapper
+} from '@dapps/modules/storage/reducer'
 import { transactionReducer as transaction } from '@dapps/modules/transaction/reducer'
 import { invitesReducer as invite } from 'modules/invite/reducer'
 
 // TODO: Consider spliting individual reducers into { data, loading, error }
-export const rootReducer = combineReducers<RootState>({
-  router,
-  wallet,
-  translation,
-  storage,
-  transaction,
-  invite
-})
+export const rootReducer = storageReducerWrapper(
+  combineReducers<RootState>({
+    router,
+    wallet,
+    translation,
+    storage,
+    transaction,
+    invite
+  })
+)
